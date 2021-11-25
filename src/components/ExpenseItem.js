050209e6d331681fb05cf6517a92e2e876b4da99
MateuszK0s <kosmateusz99@gmail.com>
2021-11-25 17:45:48 +0100

    Added unique wrapper component named Card

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,18 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const { date, title, amount } = props.expense;
 
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
-  const month = date.toLocaleString("en-US", { month: "long" });
-  const year = date.getFullYear();
-
   return (
-    <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
